c611c252e77522f77865d1c89647a5d717155804
pandit <shrutikumaripandit@gmail.com>
2024-09-13 14:35:21 +0530

    Commited by shruti

--- app/src/main/java/com/sk/apipracticetoadvancelevelbysk/adapter/UserAdapter.kt
+++ app/src/main/java/com/sk/apipracticetoadvancelevelbysk/adapter/UserAdapter.kt
@@ -17,6 +17,7 @@ class UserAdapter(private val users: List<UserX>) : RecyclerView.Adapter<UserAda
         val userName: TextView = itemView.findViewById(R.id.userName)
         val userEmail: TextView = itemView.findViewById(R.id.userEmail)
         val userPhone: TextView = itemView.findViewById(R.id.userPhone)
+        val userpassword: TextView = itemView.findViewById(R.id.userpassword)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
@@ -27,8 +28,11 @@ class UserAdapter(private val users: List<UserX>) : RecyclerView.Adapter<UserAda
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
         val user = users[position]
         holder.userName.text = "${user.firstName} ${user.lastName}"
-        holder.userEmail.text = user.email
+
+        holder.userName.setOnClickListener { holder.userEmail.text = user.email
         holder.userPhone.text = user.phone
+        holder.userpassword.text = user.password
+        }
 
         // Load user image using Glide
         Glide.with(holder.itemView.context)
